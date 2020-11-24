8f3356d4c47a3244f5a6e972213426fa9e541368
ozanyil1 <60675637+ozanyil1@users.noreply.github.com>
2020-11-25 00:32:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ selectedFile.onchange = function(){
             if (i===-1){
                 td.innerHTML = "Genel Toplam"} else {
                     for(b=0;b<Object.keys(table).length;b++){
-                        if (Object.keys(table)[b]=>7000&&Object.keys(table)[b]<8000){
+                        if (Object.keys(table)[b]>=7000&&Object.keys(table)[b]<8000){
                             //console.log(table[Object.keys(table)[b]][tableheadings[i]])
                             
                             if(table[Object.keys(table)[b]][tableheadings[i]] !== undefined) {
